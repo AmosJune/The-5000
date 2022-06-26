f9c06389351fa7883b9f29a0462b88b7508f4f29
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-06-26 08:01:46 +0300

    add an event listener, DOMContentLoaded

--- assets/index.js
+++ assets/index.js
@@ -3,13 +3,19 @@ const submit = document.getElementById('btn');
 submit.addEventListener('click', function(event){
     event.preventDefault()
     let meal = {
-        mealName : event.target.mealName.value,
-        mealImage : event.target.image_url.value,
-        mealDescription : event.target.mealDescription.value
+        mealName : event.target.strCategory.value,
+        mealImage : event.target.strCategoryThumb_url.value,
+        mealDescription : event.target.strCategoryDescription.value
     }
     listDishes(meal)
     orderDish(meal)
 });
+
+// const search = document.getElementsByTagName('form')
+// document.addEventListener('DOMContentLoaded', event => {
+//     allRecipes()
+// })
+
 
 // DOM Functions
 function listDishes(recipe){
@@ -23,20 +29,23 @@ function listDishes(recipe){
        <p>${recipe.mealDescription}</p>
       
      </div>`
-     //Append it to the DOM
+//Append it to the DOM
    document.getElementById('menuList').appendChild(meal)
 }
 
 //Fetch request
 function allRecipes(){
-    fetch("http://localhost:3000/categories")
+    fetch('http://www.themealdb.com/api/json/v1/1/categories.php')
     .then((resp) => resp.json())
     .then(recipeList => recipeList.forEach(recipe => listDishes(recipe)))
+    .catch(e => {
+        console.log(e);
+    })
  };
  
  // POST Request - Adding a meal
 function orderDish(meal){
-    fetch('http://localhost:3000/categories', {
+    fetch('http://www.themealdb.com/api/json/v1/1/categories.php', {
         method: 'POST',
         headers: {
             'Content-Type' : 'application/json'
@@ -52,3 +61,10 @@ function init(){
     allRecipes()
  };
  init();
+
+ function findMeal(searchMeal) {
+     fetch(`http://www.themealdb.com/api/json/v1/1/categories/${searchMeal.toLowerCase()}.php`)
+     .then(resp => resp.json())
+     .then(data => {
+         listDishes(data.recipe)})
+ }
